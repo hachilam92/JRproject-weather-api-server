739eb6e3085256418a0213c0f0a6921cad01d264
Ziwei Lan <ziweilan92@gmail.com>
2020-08-04 16:55:03 +1000

    Remove test codes

--- src/index.js
+++ src/index.js
@@ -8,9 +8,6 @@ app.use(express.json());
 
 // app.use(setHeader);
 app.use('/', setHeader, routes);
-app.use((err, req, res, next) => {
-    return  res.status(499).send('Unable to handle the request');
-});
 
 app.listen(PORT, () => {
     console.log(`server listening on port ${PORT}`);
